f5ce29ccaa854b33eabc092747197eef6554e4ee
Mukunda Johnson <mukunda@mukunda.com>
2025-03-04 01:09:56 -0600

    SIEVE implementation

--- sieve/sieve.go
+++ sieve/sieve.go
@@ -3,19 +3,37 @@
 // Licensed under MIT. See LICENSE file.
 //////////////////////////////////////////////////////////////////////////////////////////
 
-// SIEVE cache implementation.
-// https://junchengyang.com/publication/nsdi24-SIEVE.pdf
+/*
+This package provides a SIEVE cache implementation.
+https://junchengyang.com/publication/nsdi24-SIEVE.pdf
+
+This implementation includes a time-to-live (TTL) option for values.
+
+One implementation specific detail is how the cache handles setting a key multiple times.
+When updating an existing key, the "visited" flag is not set. If the value has a TTL, then
+the value is moved to the head.
+*/
 package sieve
 
 import "time"
 
+// For [sieveRecord.vexpires].
+const kVeVisited = 1
+const kVeExpires = 0xFFFFFFFE
+
+// For debug purposes. Should always be 0 unless there is a bug.
+var ErrFlag uint32 = 0
+
+// This returns the current unixtime. Can be overwritten by tests.
 var Now = func() int64 {
 	return time.Now().Unix()
 }
 
+// All records are allocated upfront
 type sieveRecord struct {
-	prev     uint16
-	next     uint16
+	key      string
+	prev     uint16 // 0 = null
+	next     uint16 // 0 = null
 	value    any
 	vexpires uint32 // bit0 = Visited, bit1-31 = Expiration
 }
@@ -27,8 +45,8 @@ type Sieve struct {
 	// Map of all keys to data records.
 	keys map[string]uint16
 
-	// 0 is a dummy record to make addressing less error-prone. 1-maxKeys are valid
-	// records. 0 is the "null" address.
+	// 0 is a dummy record to make addressing easier. 1-maxKeys are valid records. 0 is the
+	// "null" address.
 	data []sieveRecord
 
 	// Linked list of active records.
@@ -43,12 +61,7 @@ type Sieve struct {
 	nextFree uint16
 }
 
-type shortQueue struct {
-	head uint16
-	tail uint16
-	data []sieveRecord
-}
-
+// Remove a record from the free list and return its ID.
 func (s *Sieve) popFree() uint16 {
 	if s.nextFree == 0 {
 		return 0
@@ -62,12 +75,110 @@ func (s *Sieve) popFree() uint16 {
 	return rec
 }
 
+// Add a record by ID to the free list.
 func (s *Sieve) pushFree(rec uint16) {
 	s.data[rec].prev = 0
 	s.data[rec].next = s.nextFree
 	s.nextFree = rec
 }
 
+// Remove a record by ID from the active set. Does not clear `value` in case we want to
+// readd it.
+func (s *Sieve) pop(rec uint16) {
+	// If we are the hand, progress it.
+	datarec := &s.data[rec]
+
+	if s.hand == rec {
+		s.hand = datarec.prev
+	}
+
+	if datarec.prev != 0 {
+		s.data[datarec.prev].next = datarec.next
+	} else {
+		s.head = datarec.next
+	}
+
+	if datarec.next != 0 {
+		s.data[datarec.next].prev = datarec.prev
+	} else {
+		s.tail = datarec.prev
+	}
+
+	datarec.prev = 0
+	datarec.next = 0
+}
+
+// Add a record by ID to the active set.
+func (s *Sieve) push(rec uint16) {
+	if s.head != 0 {
+		s.data[s.head].prev = rec
+	}
+
+	s.data[rec].prev = 0
+	s.data[rec].next = s.head
+	s.head = rec
+
+	if s.tail == 0 {
+		s.tail = rec
+	}
+}
+
+// Remove a record from the active set using the SIEVE algorithm and return its ID.
+// We also check the tail first to see if the ttl is expired. If all keys use the same
+// expiration, then they will be in expiration order.
+func (s *Sieve) evict() uint16 {
+	{
+		rec := s.tail
+		if rec == 0 {
+			// sanity check - should not be reachable
+			ErrFlag |= 1
+			return 0
+		}
+		datarec := &s.data[rec]
+		exp := datarec.vexpires >> 1
+		if exp != 0 && Now() >= s.epoch+int64(exp) {
+			if s.hand == rec {
+				s.hand = datarec.prev
+			}
+			s.pop(rec)
+			datarec.value = nil
+			delete(s.keys, datarec.key)
+			return rec
+		}
+	}
+
+	if s.hand == 0 {
+		// Start a new round.
+		s.hand = s.tail
+	}
+
+	rec := s.hand
+	now := Now()
+	for {
+		datarec := &s.data[rec]
+		exp := datarec.vexpires >> 1
+		if exp != 0 && now >= s.epoch+int64(exp) {
+			// This record is expired, use it.
+		} else if datarec.vexpires&kVeVisited == 0 {
+			// This record has not been visited. use it.
+		} else {
+			datarec.vexpires &= kVeExpires
+			rec = datarec.prev
+			if rec == 0 {
+				// Start a new round.
+				rec = s.tail
+			}
+			continue
+		}
+		s.hand = datarec.prev
+		s.pop(rec)
+		datarec.value = nil
+		delete(s.keys, datarec.key)
+		return rec
+	}
+}
+
+// Create a new SIEVE cache with the given max amount of keys.
 func NewSieve(maxKeys uint16) *Sieve {
 	s := &Sieve{
 		maxKeys: maxKeys,
@@ -77,37 +188,93 @@ func NewSieve(maxKeys uint16) *Sieve {
 	return s
 }
 
-func (s *Sieve) Set(key string, value any) {
-	// TODO
+// Returns a key from the cache. If it is not found, returns nil.
+func (s *Sieve) Get(key string) any {
+	rec := s.keys[key]
+	if rec == 0 {
+		return nil
+	}
+
+	datarec := &s.data[rec]
+	exp := datarec.vexpires >> 1
+	if exp != 0 && Now() >= s.epoch+int64(exp) {
+		// Expired.
+		if s.hand == rec {
+			s.hand = datarec.prev
+		}
+		s.pop(rec)
+		s.pushFree(rec)
+		datarec.value = nil
+		delete(s.keys, datarec.key)
+		return nil
+	}
+
+	// Set the visited flag.
+	datarec.vexpires |= kVeVisited
+	return datarec.value
 }
 
-func (s *Sieve) Delete(key string) {
-	entry := s.keys[key]
-	if entry == 0 {
+func (s *Sieve) translateExpiresValue(ttl time.Duration) uint32 {
+	if ttl == 0 {
+		return 0
+	}
+	return uint32((Now() - s.epoch + int64(ttl.Seconds())) << 1)
+}
+
+// Update a value in the cache by key. `ttl` is an expiration time in seconds. 0 = no
+// expiration.
+func (s *Sieve) Set(key string, value any, ttl time.Duration) {
+	rec := s.keys[key]
+	if rec != 0 {
+
+		// Entry exists in the cache. Update the value. If ttl is set, then bump it to the
+		// top. Otherwise, leave it where it is. These operations are not defined in the
+		// paper (overwriting or ttl behavior).
+		s.data[rec].value = value
+		s.data[rec].vexpires = s.data[rec].vexpires&kVeVisited |
+			s.translateExpiresValue(ttl)
+
+		if ttl != 0 {
+			// Move to front if TTL is present.
+			s.pop(rec)
+			s.push(rec)
+		} else {
+			// It may not be beneficial to mark as visited if it's not being read.
+			//s.data[rec].vexpires |= kVeVisited
+		}
 		return
 	}
 
-	delete(s.keys, key)
-
-	data := s.data[entry]
-	if data.prev != 0 {
-		s.data[data.prev].next = data.next
-	}
-	if data.next != 0 {
-		s.data[data.next].prev = data.prev
-	}
-
-	if s.head == entry {
-		s.head = data.next
-	}
-	if s.tail == entry {
-		s.tail = data.prev
+	rec = s.popFree()
+	if rec == 0 {
+		rec = s.evict()
+		if rec == 0 {
+			// sanity check - should not be reachable
+			ErrFlag |= 2
+			return
+		}
 	}
 
-	s.data[entry].value = nil
-	s.pushFree(entry)
+	s.keys[key] = rec
+	s.data[rec].key = key
+	s.data[rec].value = value
+	s.data[rec].vexpires = s.translateExpiresValue(ttl)
+	s.push(rec)
 }
 
+// Invalidate a value from the cache by key. Does nothing if the key doesn't exist.
+func (s *Sieve) Delete(key string) {
+	rec := s.keys[key]
+	if rec == 0 {
+		return
+	}
+	delete(s.keys, key)
+	s.pop(rec)
+	s.data[rec].value = nil
+	s.pushFree(rec)
+}
+
+// Release all keys and reset the cache to an empty state.
 func (s *Sieve) Reset() {
 	s.head = 0
 	s.tail = 0
@@ -119,5 +286,20 @@ func (s *Sieve) Reset() {
 
 	for i := uint16(1); i <= s.maxKeys; i++ {
 		s.pushFree(i)
+	}
+}
+
+// Delete all expired keys.
+func (s *Sieve) Clean() {
+	for rec := s.head; rec != 0; {
+		next := s.data[rec].next
+		exp := s.data[rec].vexpires >> 1
+		if exp != 0 && Now() >= s.epoch+int64(exp) {
+			s.pop(rec)
+			s.data[rec].value = nil
+			delete(s.keys, s.data[rec].key)
+			s.pushFree(rec)
+		}
+		rec = next
 	}
 }
--- sieve/sieve_test.go
+++ sieve/sieve_test.go
@@ -6,13 +6,476 @@
 package sieve_test
 
 import (
+	"container/list"
+	"fmt"
+	"math/rand"
 	"testing"
+	"time"
 
 	"go.mukunda.com/oncache/sieve"
 )
 
-func Test1(t *testing.T) {
+// We'll test against an alternate implementation.
+type tSieve struct {
+	List    list.List
+	Keys    map[string]*list.Element
+	Hand    *list.Element
+	MaxKeys int
+}
+
+type tElement struct {
+	key     string
+	value   int
+	expires int64
+	visited bool
+}
+
+func newTestSieve(maxKeys int) *tSieve {
+	return &tSieve{
+		Keys:    make(map[string]*list.Element),
+		MaxKeys: maxKeys,
+	}
+}
+
+func (ts *tSieve) Set(key string, value int, ttl time.Duration) {
+	elem := ts.Keys[key]
+	if elem != nil {
+
+		elem.Value.(*tElement).value = value
+		if ttl != 0 {
+			elem.Value.(*tElement).expires = sieve.Now() + int64(ttl.Seconds())
+			if ts.Hand == elem {
+				ts.Hand = ts.Hand.Prev()
+			}
+			ts.List.MoveToFront(elem)
+		} else {
+			elem.Value.(*tElement).expires = 0
+		}
+
+		return
+	}
+
+	if ts.List.Len() >= ts.MaxKeys {
+		ts.evict()
+	}
+	expires := sieve.Now() + int64(ttl.Seconds())
+	if ttl == 0 {
+		expires = 0
+	}
+	ts.Keys[key] = ts.List.PushFront(&tElement{key, value, expires, false})
+}
+
+func (ts *tSieve) Get(key string) int {
+	elem, ok := ts.Keys[key]
+	if !ok {
+		return -1
+	}
+
+	if elem.Value.(*tElement).expires != 0 && sieve.Now() >= elem.Value.(*tElement).expires {
+		ts.discard(elem)
+		return -1
+	}
+
+	elem.Value.(*tElement).visited = true
+	return elem.Value.(*tElement).value
+}
+
+func (ts *tSieve) evict() {
+	{
+		// Additional implementation details: remove tail if it's expired. As per [7.3] in
+		// the paper, the SIEVE algorithm is TTL-friendly because it maintains objects in
+		// insertion order. If all keys have the same TTL, then the tail will always expire
+		// first.
+		o := ts.List.Back()
+		if o == nil {
+			panic("evict on empty list")
+		}
+		if o.Value.(*tElement).expires != 0 && sieve.Now() >= o.Value.(*tElement).expires {
+			ts.discard(o)
+			return
+		}
+	}
+
+	if ts.Hand == nil {
+		ts.Hand = ts.List.Back()
+	}
+
+	for ts.Hand.Value.(*tElement).visited &&
+		(ts.Hand.Value.(*tElement).expires == 0 || sieve.Now() < ts.Hand.Value.(*tElement).expires) {
+		ts.Hand.Value.(*tElement).visited = false
+		ts.Hand = ts.Hand.Prev()
+		if ts.Hand == nil {
+			ts.Hand = ts.List.Back()
+		}
+	}
+
+	ts.discard(ts.Hand)
+}
+
+func (ts *tSieve) discard(elem *list.Element) {
+	if ts.Hand == elem {
+		ts.Hand = elem.Prev()
+	}
+	ts.List.Remove(elem)
+	delete(ts.Keys, elem.Value.(*tElement).key)
+}
+
+func (ts *tSieve) Clean() {
+	for e := ts.List.Front(); e != nil; {
+		next := e.Next()
+		if e.Value.(*tElement).expires != 0 && sieve.Now() >= e.Value.(*tElement).expires {
+			ts.discard(e)
+		}
+		e = next
+	}
+}
+
+func assert(t *testing.T, cond bool) {
+	t.Helper()
+	if !cond {
+		t.Fatalf("Assertion failed.")
+	}
+}
+
+func TestSieveBasic(t *testing.T) {
+	s := sieve.NewSieve(3)
+
+	// [SPEC] When no expiration is used, the cache follows the SIEVE algorithm directly.
+
+	// []
+	//    ↑ (hand)
+	assert(t, s.Get("a") == nil)
+	s.Set("a", 1, 0)
+	// [a]
+	//     ↑
+	assert(t, s.Get("b") == nil)
+	s.Set("b", 2, 0)
+	// [b, a]
+	//        ↑
+	assert(t, s.Get("c") == nil)
+	s.Set("c", 3, 0)
+	// [c, b, a]
+	//           ↑
+
+	// [c, b, ^a] ^ = visited
+	//            ↑
+	assert(t, s.Get("a").(int) == 1)
+
+	assert(t, s.Get("d") == nil)
+	s.Set("d", 4, 0)
+	// [d, c, ^a]
+	//         ↑ skip visited
+	// [c, b, a]
+	//     ↑ evict b
+	// [d, c, a]
+	//     ↑
+
+	assert(t, s.Get("a") == 1)
+	assert(t, s.Get("b") == nil)
+	// [d, c, a^]
+	//     ↑
+
+	assert(t, s.Get("e") == nil)
+	s.Set("e", 5, 0)
+	assert(t, s.Get("c") == nil) // evicted
+	// [e, d, a^]
+	//     ↑
+
+	assert(t, s.Get("a") == 1)
+	assert(t, s.Get("d") == 4)
+	assert(t, s.Get("e") == 5)
+	// [e^, d^, a^]
+	//      ↑
+
+	assert(t, s.Get("f") == nil)
+	s.Set("f", 6, 0)
+
+	// [e^, d^, a^]
+	//      ↑
+	// [e^, d, a^]
+	//  ↑
+	// [e, d, a^]
+	//        ↑
+	// [e, d, a]
+	//     ↑
+	// [f, e, a]
+	//     ↑
+	assert(t, s.Get("d") == nil) // evicted
+
+	assert(t, s.Get("a") == 1)
+	assert(t, s.Get("e") == 5)
+	assert(t, s.Get("f") == 6)
+}
+
+func TestTestSieveBasic(t *testing.T) {
+	// Testing the same as above on the test sieve.
+	s := newTestSieve(3)
+
+	assert(t, s.Get("a") == -1)
+	s.Set("a", 1, 0)
+	assert(t, s.Get("b") == -1)
+	s.Set("b", 2, 0)
+	assert(t, s.Get("c") == -1)
+	s.Set("c", 3, 0)
+	assert(t, s.Get("a") == 1)
+	assert(t, s.Get("d") == -1)
+	s.Set("d", 4, 0)
+	assert(t, s.Get("a") == 1)
+	assert(t, s.Get("b") == -1)
+	assert(t, s.Get("e") == -1)
+	s.Set("e", 5, 0)
+	assert(t, s.Get("c") == -1)
+	assert(t, s.Get("a") == 1)
+	assert(t, s.Get("d") == 4)
+	assert(t, s.Get("e") == 5)
+	assert(t, s.Get("f") == -1)
+	s.Set("f", 6, 0)
+	assert(t, s.Get("d") == -1)
+	assert(t, s.Get("a") == 1)
+	assert(t, s.Get("e") == 5)
+	assert(t, s.Get("f") == 6)
+}
+
+func TestSieveRandomSetBatchNoExpiration(t *testing.T) {
+
+	for i := 0; i < 2000; i++ {
+		ts := newTestSieve(10)
+		rs := sieve.NewSieve(10)
+
+		for j := 0; j < 100; j++ {
+			key := fmt.Sprintf("key%d", rand.Intn(20))
+			value := rand.Intn(10)
+			ts.Set(key, value, 0)
+			rs.Set(key, value, 0)
+		}
+
+		for j := 0; j < 20; j++ {
+			key := fmt.Sprintf("key%d", j)
+			val := ts.Get(key)
+			rval := rs.Get(key)
+			if val != -1 {
+				if val != rval {
+					t.Fatalf("Mismatched value for key %s: %d, expected %d", key, rval, val)
+				}
+			} else {
+				if rval != nil {
+					t.Fatalf("Mismatched value for key %s: %d, expected %d", key, rval, val)
+				}
+			}
+		}
+	}
+}
+
+func TestSieveRandomCacheBatchNoExpiration(t *testing.T) {
+	// Testing cache emulation of random 20 keys with 10 cache slots.
+	// Each operation is GET attempt with following SET if not found.
+
+	for i := 0; i < 2000; i++ {
+		ts := newTestSieve(10)
+		rs := sieve.NewSieve(10)
+
+		for j := 0; j < 100; j++ {
+			item := rand.Intn(20)
+			key := fmt.Sprintf("key%d", item)
+
+			tsget := ts.Get(key)
+			if tsget != -1 {
+				assert(t, tsget == item)
+			} else {
+				ts.Set(key, item, 0)
+			}
+
+			rsget := rs.Get(key)
+			if rsget != nil {
+				assert(t, tsget != -1)
+				assert(t, rsget == item)
+				assert(t, rsget.(int) == tsget)
+			} else {
+				rs.Set(key, item, 0)
+			}
+		}
+
+		for j := 0; j < 20; j++ {
+			key := fmt.Sprintf("key%d", j)
+			val := ts.Get(key)
+			rval := rs.Get(key)
+			if val != -1 {
+				if val != rval {
+					t.Fatalf("Mismatched value for key %s: %d, expected %d", key, rval, val)
+				}
+			} else {
+				if rval != nil {
+					t.Fatalf("Mismatched value for key %s: %d, expected %d", key, rval, val)
+				}
+			}
+		}
+	}
+}
+
+var nowRestore = sieve.Now
+var testTime int64
+var testTimeBase = int64(1735689600) // 2025-01-01 00:00:00 UTC
+
+func useTime(t int64) {
+	testTime = t
 	sieve.Now = func() int64 {
-		return 0
+		return testTimeBase + testTime
+	}
+}
+
+func advanceTime(d int64) {
+	testTime += d
+}
+
+func restoreTime() {
+	sieve.Now = nowRestore
+}
+
+func TestSieveTTLBasics(t *testing.T) {
+	useTime(0)
+	defer restoreTime()
+
+	// [SPEC] If a key is expired, then it is removed from the list when an access is
+	//        attempted. Nil is returned.
+
+	ts := sieve.NewSieve(3)
+	ts.Set("a", 1, 1*time.Second)
+	assert(t, ts.Get("a") == 1)
+	advanceTime(1)
+	assert(t, ts.Get("a") == nil)
+	ts.Set("a", 1, 2*time.Second)
+	advanceTime(1)
+	assert(t, ts.Get("a") == 1)
+	advanceTime(1)
+	assert(t, ts.Get("a") == nil)
+
+	// [SPEC] When evicting keys, the tail is checked for expiration before the normal
+	//        SIEVE algorithm.
+
+	ts.Reset()
+	ts.Set("a", 1, 20*time.Second) // A at the tail.
+	advanceTime(5)
+	ts.Set("b", 2, 20*time.Second) // B in the middle.
+	advanceTime(5)
+	ts.Set("c", 3, 20*time.Second) // C in the front.
+
+	ts.Get("a")                    // Bump A.
+	ts.Set("d", 4, 20*time.Second) // D will evict B, putting the hand in the middle. A  is skipped (visited).
+	assert(t, ts.Get("b") == nil)
+
+	advanceTime(10) // A is expired now, so A at the tail will be evicted, and not C.
+	ts.Set("e", 5, 20*time.Second)
+	assert(t, ts.Get("a") == nil)
+	assert(t, ts.Get("c") == 3)
+	assert(t, ts.Get("d") == 4)
+	assert(t, ts.Get("e") == 5)
+}
+
+func TestTestSieveTTLBasics(t *testing.T) {
+	useTime(0)
+	defer restoreTime()
+
+	// [SPEC] If a key is expired, then it is removed from the list when an access is
+	//        attempted. Nil is returned.
+
+	ts := newTestSieve(3)
+	ts.Set("a", 1, 1*time.Second)
+	assert(t, ts.Get("a") == 1)
+	advanceTime(1)
+	assert(t, ts.Get("a") == -1)
+	ts.Set("a", 1, 2*time.Second)
+	advanceTime(1)
+	assert(t, ts.Get("a") == 1)
+	advanceTime(1)
+	assert(t, ts.Get("a") == -1)
+
+	// [SPEC] When evicting keys, the tail is checked for expiration before the normal
+	//        SIEVE algorithm.
+
+	ts = newTestSieve(3)
+	ts.Set("a", 1, 20*time.Second) // A at the tail.
+	advanceTime(5)
+	ts.Set("b", 2, 20*time.Second) // B in the middle.
+	advanceTime(5)
+	ts.Set("c", 3, 20*time.Second) // C in the front.
+
+	ts.Get("a")                    // Bump A.
+	ts.Set("d", 4, 20*time.Second) // D will evict B, putting the hand in the middle. A  is skipped (visited).
+	assert(t, ts.Get("b") == -1)
+
+	advanceTime(10) // A is expired now, so A at the tail will be evicted, and not C.
+	ts.Set("e", 5, 20*time.Second)
+	assert(t, ts.Get("a") == -1)
+	assert(t, ts.Get("c") == 3)
+	assert(t, ts.Get("d") == 4)
+	assert(t, ts.Get("e") == 5)
+}
+
+func TestSievesWithFixedTtl(t *testing.T) {
+	useTime(0)
+	defer restoreTime()
+
+	for i := 0; i < 5; i++ {
+		rnd := rand.New(rand.NewSource(int64(i)))
+
+		ts := newTestSieve(10)
+		rs := sieve.NewSieve(10)
+
+		for j := 0; j < 10000; j++ {
+			item := rnd.Intn(15)
+			key := fmt.Sprintf("key%d", item)
+
+			tsItem := ts.Get(key)
+			rsItem := rs.Get(key)
+
+			if tsItem == -1 {
+				assert(t, rsItem == nil)
+				ts.Set(key, item, 10*time.Second)
+				rs.Set(key, item, 10*time.Second)
+			} else {
+				assert(t, tsItem == rsItem)
+			}
+			advanceTime(int64(rnd.Intn(10)))
+		}
+	}
+}
+
+func TestSievesWithRandomTtl(t *testing.T) {
+	useTime(0)
+	defer restoreTime()
+
+	for i := 0; i < 10; i++ {
+		cleaning := i >= 5
+
+		rnd := rand.New(rand.NewSource(int64(i)))
+
+		ts := newTestSieve(10)
+		rs := sieve.NewSieve(10)
+
+		for j := 0; j < 10000; j++ {
+
+			item := rnd.Intn(15)
+			key := fmt.Sprintf("key%d", item)
+
+			tsItem := ts.Get(key)
+			rsItem := rs.Get(key)
+
+			if tsItem == -1 {
+				assert(t, rsItem == nil)
+				ttl := time.Duration(rnd.Intn(15)) * time.Second
+				ts.Set(key, item, ttl)
+				rs.Set(key, item, ttl)
+			} else {
+				assert(t, tsItem == rsItem)
+			}
+			advanceTime(int64(rnd.Intn(10)))
+			if cleaning {
+				// For iterations 5-10, do cleaning every 2 iterations
+				if j%2 == 1 {
+					ts.Clean()
+					rs.Clean()
+				}
+			}
+		}
 	}
 }
